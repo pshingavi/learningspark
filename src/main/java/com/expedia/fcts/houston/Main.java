5e7fd08f3f7cf6e37855948565090a8e4b90d96f
Preetam Shingavi <pshingavi@expedia.com>
2019-03-21 16:34:00 -0700

    Read from file in a RDD

--- src/main/java/com/expedia/fcts/houston/Main.java
+++ src/main/java/com/expedia/fcts/houston/Main.java
@@ -1,28 +1,16 @@
 package com.expedia.fcts.houston;
 
-import com.google.common.collect.Iterables;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import scala.Tuple2;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        List<String> inputData = new ArrayList<>();
-        inputData.add("WARN: Tuesday 4 September 0405");
-        inputData.add("ERROR: Tuesday 4 September 0408");
-        inputData.add("FATAL: Wednesday 5 September 0415");
-        inputData.add("ERROR: Friday 7 September 2405");
-        inputData.add("WARN: Saturday 8 September 3405");
 
         // Set logging
         Logger.getLogger("org.apache").setLevel(Level.WARN);
@@ -35,10 +23,11 @@ public class Main {
         // Connect to spark
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
 
-        sc.parallelize(inputData)
-                .flatMap(currStr ->
-                Arrays.asList(currStr.split(" ")).iterator())
-                .filter(word -> word.length() > 1)
+        // Read from file
+        // Does not load in memory immediately. Tells the worker nodes to load in parts
+        JavaRDD<String> initialRDD = sc.textFile("src/main/resources/input.txt");
+        initialRDD
+                .flatMap(sentence -> Arrays.asList(sentence.split(" ")).iterator())
                 .collect().forEach(System.out::println);
 
         sc.close();
