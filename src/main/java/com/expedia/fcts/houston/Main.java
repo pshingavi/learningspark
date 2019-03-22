6fc20a830329a0b16d81b553904783af27c2fd7b
Preetam Shingavi <pshingavi@expedia.com>
2019-03-21 18:22:10 -0700

    Read from file and find top 10 most occuring words

--- src/main/java/com/expedia/fcts/houston/Main.java
+++ src/main/java/com/expedia/fcts/houston/Main.java
@@ -1,10 +1,11 @@
 package com.expedia.fcts.houston;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
 
 import java.util.Arrays;
 
@@ -12,10 +13,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Set logging
+        // Set loggin level for apache
         Logger.getLogger("org.apache").setLevel(Level.WARN);
 
-        // Create spark conf
+        // Setup spark config
         SparkConf sparkConf = new SparkConf()
                 .setAppName("mySparkApp")
                 .setMaster("local[*]");
@@ -23,12 +24,22 @@ public class Main {
         // Connect to spark
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
 
-        // Read from file
-        // Does not load in memory immediately. Tells the worker nodes to load in parts
-        JavaRDD<String> initialRDD = sc.textFile("src/main/resources/input.txt");
-        initialRDD
-                .flatMap(sentence -> Arrays.asList(sentence.split(" ")).iterator())
-                .collect().forEach(System.out::println);
+        sc.textFile("src/main/resources/input.txt")
+                .flatMap(sentence -> Arrays.asList(sentence
+                            .replaceAll("[^a-zA-z\\s]", "")
+                            .toLowerCase()
+                            .trim()
+                            .split(" ")
+                    ).iterator()
+                )
+                .filter(word -> !StringUtils.isEmpty(word))
+                .mapToPair(word -> new Tuple2<>(word, 1))
+                .reduceByKey((x, y) -> x+y)
+                // Need below to switch key and value to use sortByKey
+                .mapToPair(tuple -> new Tuple2<> (tuple._2, tuple._1))
+                .sortByKey(false)
+                .take(10)
+                .forEach(tuple -> System.out.println(tuple._1 + ": " + tuple._2));
 
         sc.close();
     }
