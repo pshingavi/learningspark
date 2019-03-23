b6c3acba385c260ad86ae768a88cd1befee9962d
Preetam Shingavi <pshingavi@expedia.com>
2019-03-23 12:47:08 -0700

    Note on foreach with sort
    Do not serealize foreach for sort

--- src/main/java/com/expedia/fcts/houston/Main.java
+++ src/main/java/com/expedia/fcts/houston/Main.java
@@ -20,6 +20,7 @@ public class Main {
         SparkConf sparkConf = new SparkConf()
                 .setAppName("mySparkApp")
                 .setMaster("local[*]");
+        // Remove setMaster when deploying to real cluster else will keep running everything in the driver node
 
         // Connect to spark
         JavaSparkContext sc = new JavaSparkContext(sparkConf);
@@ -38,6 +39,7 @@ public class Main {
                 // Need below to switch key and value to use sortByKey
                 .mapToPair(tuple -> new Tuple2<> (tuple._2, tuple._1))
                 .sortByKey(false)
+                // If you use foreach here, results might be incorrect. Using coalesce(#num of partition) is also not a great idea. It won't work
                 .take(10)
                 .forEach(tuple -> System.out.println(tuple._1 + ": " + tuple._2));
 
