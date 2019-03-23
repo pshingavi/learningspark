6f5bda175e32ef73c74efb47bfd6c0186be4b3ae
Preetam Shingavi <pshingavi@expedia.com>
2019-03-23 15:20:37 -0700

    Partial implementation of exercise - Hot course
    Find the hottest course

---
+++ src/main/java/com/expedia/fcts/houston/ViewingFigures.java
@@ -0,0 +1,137 @@
+package com.expedia.fcts.houston;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class is used in the chapter late in the course where we analyse viewing figures.
+ * You can ignore until then.
+ */
+public class ViewingFigures 
+{
+	public static void main(String[] args)
+	{
+		//System.setProperty("hadoop.home.dir", "/Users/pshingavi/dev/mysparklearning/hadoop_home/hadoop");
+		Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+		SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
+		JavaSparkContext sc = new JavaSparkContext(conf);
+		
+		// Use true to use hardcoded data identical to that in the PDF guide.
+		boolean testMode = false;
+		
+		JavaPairRDD<Integer, Integer> viewData = setUpViewDataRdd(sc, testMode);
+		JavaPairRDD<Integer, Integer> chapterData = setUpChapterDataRdd(sc, testMode);
+		JavaPairRDD<Integer, String> titlesData = setUpTitlesDataRdd(sc, testMode);
+
+		// Warm up - Find which course has how many chapters. Returns (courseId, countOfChapters)
+        System.out.println("Course and chapter count");
+        chapterData
+                .mapToPair(tuple -> new Tuple2<>(tuple._2, 1))
+                .reduceByKey((x, y) -> x+y)
+                .collect()
+                .forEach(System.out::println);
+
+        // Step 1 - Remove duplicate views. Returns unique (userId, chapterId)
+        viewData = viewData.distinct();
+        System.out.println("View data RDD");
+        viewData.collect().forEach(System.out::println);
+
+        System.out.println("Most hot course");
+        // Step 2 - Join (chapterId, userId) with (chapterId, courseId) -> (chapterId, (userId, courseId))
+        JavaPairRDD<Integer, Tuple2<Integer, Integer>> chapterCourseRDD = viewData
+                .distinct()
+                .mapToPair(t -> new Tuple2<>(t._2, t._1))
+                .join(chapterData);
+
+        chapterCourseRDD
+                .mapToPair(t -> new Tuple2<>(t._2._2, 1))
+                .reduceByKey((x, y) -> x+y)
+                .mapToPair(t -> new Tuple2<>(t._2, t._1))
+                .sortByKey(false)
+                .mapToPair(t -> new Tuple2<>(t._2, t._1))
+                .take(1)
+                .forEach(res -> System.out.println("Course " + res._1 + " is hotest with " + res._2 + " views"));
+
+		
+		sc.close();
+	}
+
+	private static JavaPairRDD<Integer, String> setUpTitlesDataRdd(JavaSparkContext sc, boolean testMode) {
+		
+		if (testMode)
+		{
+			// (chapterId, title)
+			List<Tuple2<Integer, String>> rawTitles = new ArrayList<>();
+			rawTitles.add(new Tuple2<>(1, "How to find a better job"));
+			rawTitles.add(new Tuple2<>(2, "Work faster harder smarter until you drop"));
+			rawTitles.add(new Tuple2<>(3, "Content Creation is a Mug's Game"));
+			return sc.parallelizePairs(rawTitles);
+		}
+		return sc.textFile("src/main/resources/viewing figures/titles.csv")
+				                                    .mapToPair(commaSeparatedLine -> {
+														String[] cols = commaSeparatedLine.split(",");
+														return new Tuple2<Integer, String>(new Integer(cols[0]),cols[1]);
+				                                    });
+	}
+
+	private static JavaPairRDD<Integer, Integer> setUpChapterDataRdd(JavaSparkContext sc, boolean testMode) {
+		
+		if (testMode)
+		{
+			// (chapterId, (courseId, courseTitle))
+			List<Tuple2<Integer, Integer>> rawChapterData = new ArrayList<>();
+			rawChapterData.add(new Tuple2<>(96,  1));
+			rawChapterData.add(new Tuple2<>(97,  1));
+			rawChapterData.add(new Tuple2<>(98,  1));
+			rawChapterData.add(new Tuple2<>(99,  2));
+			rawChapterData.add(new Tuple2<>(100, 3));
+			rawChapterData.add(new Tuple2<>(101, 3));
+			rawChapterData.add(new Tuple2<>(102, 3));
+			rawChapterData.add(new Tuple2<>(103, 3));
+			rawChapterData.add(new Tuple2<>(104, 3));
+			rawChapterData.add(new Tuple2<>(105, 3));
+			rawChapterData.add(new Tuple2<>(106, 3));
+			rawChapterData.add(new Tuple2<>(107, 3));
+			rawChapterData.add(new Tuple2<>(108, 3));
+			rawChapterData.add(new Tuple2<>(109, 3));
+			return sc.parallelizePairs(rawChapterData);
+		}
+
+		return sc.textFile("src/main/resources/viewing figures/chapters.csv")
+													  .mapToPair(commaSeparatedLine -> {
+															String[] cols = commaSeparatedLine.split(",");
+															return new Tuple2<Integer, Integer>(new Integer(cols[0]), new Integer(cols[1]));
+													  	});
+	}
+
+	private static JavaPairRDD<Integer, Integer> setUpViewDataRdd(JavaSparkContext sc, boolean testMode) {
+		
+		if (testMode)
+		{
+			// Chapter views - (userId, chapterId)
+			List<Tuple2<Integer, Integer>> rawViewData = new ArrayList<>();
+			rawViewData.add(new Tuple2<>(14, 96));
+			rawViewData.add(new Tuple2<>(14, 97));
+			rawViewData.add(new Tuple2<>(13, 96));
+			rawViewData.add(new Tuple2<>(13, 96));
+			rawViewData.add(new Tuple2<>(13, 96));
+			rawViewData.add(new Tuple2<>(14, 99));
+			rawViewData.add(new Tuple2<>(13, 100));
+			return  sc.parallelizePairs(rawViewData);
+		}
+		
+		return sc.textFile("src/main/resources/viewing figures/views-*.csv")
+				     .mapToPair(commaSeparatedLine -> {
+				    	 String[] columns = commaSeparatedLine.split(",");
+				    	 return new Tuple2<Integer, Integer>(new Integer(columns[0]), new Integer(columns[1]));
+				     });
+	}
+}
