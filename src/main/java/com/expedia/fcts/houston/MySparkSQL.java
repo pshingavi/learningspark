b124f159dbde0ac1bdfb509bbd7711cec81adaab
Preetam Shingavi <pshingavi@expedia.com>
2019-03-23 20:12:43 -0700

    SparkSQL use Column.<fun>().and/or/etc for filter

--- src/main/java/com/expedia/fcts/houston/MySparkSQL.java
+++ src/main/java/com/expedia/fcts/houston/MySparkSQL.java
@@ -3,6 +3,7 @@ package com.expedia.fcts.houston;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.api.java.function.FilterFunction;
+import org.apache.spark.sql.Column;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
@@ -28,9 +29,12 @@ public class MySparkSQL {
         // Load data
         Dataset<Row> dataSet = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
 
-        Dataset<Row> resultSet = dataSet.filter((FilterFunction<Row>) row ->
-                row.getAs("subject").equals("Math"));
-        resultSet.show();
+        Column subjectColumn = dataSet.col("subject");
+        Column scoreColumn = dataSet.col("score");
+
+        Dataset<Row> resultDataSet = dataSet.filter(subjectColumn.equalTo("Math")
+        .and(scoreColumn.$greater$eq(50)));
+        resultDataSet.show();
 
         // Using Scanner to interrupt and watch the SparkUI
         /*Scanner scanner = new Scanner(System.in);
