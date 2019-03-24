92549978588f32e2eb6f2711b7fe2d22a8a1cfeb
Preetam Shingavi <pshingavi@expedia.com>
2019-03-23 19:45:40 -0700

    SparkSQL starter - Count students.csv

---
+++ src/main/java/com/expedia/fcts/houston/MySparkSQL.java
@@ -0,0 +1,41 @@
+package com.expedia.fcts.houston;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import java.util.Scanner;
+
+public class MySparkSQL {
+
+    public static void main(String[] args) {
+
+        // Set logging
+        System.setProperty("hadoop.home.dir", "/Users/pshingavi/hadoop");
+        Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+        // Spark session for SQL
+        SparkSession spark = SparkSession.builder()
+                .appName("mySparkSQL")
+                .master("local[*]")
+                // Location for temp dir used by SparkSQL
+                .config("spark.sql.warehouse.dir", "file:///Users/pshingavi/dev/mysparkleaning/tmp")
+                .getOrCreate();
+
+        // Load data
+        Dataset<Row> dataSet = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
+        dataSet.show();
+
+        // .count() is an action on the underlying RDD
+        System.out.println("Total rows: " + dataSet.count());
+
+        // Using Scanner to interrupt and watch the SparkUI
+        Scanner scanner = new Scanner(System.in);
+        scanner.next();
+
+        // Close spark connection
+        spark.close();
+    }
+}
