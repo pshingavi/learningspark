233f0664d28256ffe85dc7f2df8e58cfd78a6591
Preetam Shingavi <pshingavi@expedia.com>
2019-03-24 22:30:24 -0700

    Stream analysis on LOG level - Starter

--- src/main/java/com/expedia/fcts/houston/learnsparkstreaming/dstream/MyDStream.java
+++ src/main/java/com/expedia/fcts/houston/learnsparkstreaming/dstream/MyDStream.java
@@ -7,6 +7,7 @@ import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import scala.Tuple2;
 
 public class MyDStream {
 
@@ -15,6 +16,7 @@ public class MyDStream {
         // Logger and hadoop dir
         System.setProperty("hadoop.home.dir", "/Users/pshingavi/hadoop");
         Logger.getLogger("org.apache").setLevel(Level.WARN);
+        Logger.getLogger("org.apache.spark.storage").setLevel(Level.WARN);
 
         // Create SparkConf
         SparkConf conf = new SparkConf()
@@ -22,11 +24,14 @@ public class MyDStream {
                 .setAppName("myStreamingApp");
 
         // Set JavaStreamingContext
-        JavaStreamingContext sc = new JavaStreamingContext(conf, Durations.seconds(30));
+        JavaStreamingContext sc = new JavaStreamingContext(conf, Durations.seconds(10));
 
         JavaReceiverInputDStream<String> resultDStream =  sc.socketTextStream("localhost", 9999);
         JavaDStream<String> result = resultDStream.map(item -> item);
-        result.print();
+        result.map(rawMessage -> rawMessage.split(",")[0])
+                .mapToPair(level -> new Tuple2<>(level, 1))
+                .reduceByKey((x, y) -> x+y)
+                .print();
 
         sc.start(); // Start streaming
         sc.awaitTermination();  // Wait termination of jvm
